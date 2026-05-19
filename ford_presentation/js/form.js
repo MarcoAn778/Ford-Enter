7e19df64d6e33b8321eaf44e46b20755dafa0fa1
Marco Antônio <marco.duoli2@gmail.com>
2026-05-19 14:49:14 -0300

    Criação do formulário

--- ford_presentation/js/form.js
+++ ford_presentation/js/form.js
@@ -2,26 +2,37 @@
 //class contato
 
 class contato {
-    
+    constructor(nome, sobrenome, email, cpf, telefone, tipo_contato, mensagem) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.tipo_contato = tipo_contato;
+        this.mensagem = mensagem;
+    }
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
+    let data = new contato(
+        form.elements.namedItem("nome").value,
+        form.elements.namedItem("sobrenome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("cpf").value,
+        form.elements.namedItem("telefone").value,  
+        form.elements.namedItem("tipo_contato").value,
+        form.elements.namedItem("mensagem").value);
   
+    console.log("Dados do contato:", data);
+
+    Enviar(data.nome);
 }
 
-function Enviar() {
+function Enviar(nomeUser) {
 
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    if (nomeUser && nomeUser.trim() !== "") {
+        alert('Obrigado sr(a) ' + nomeUser + ' os seus dados foram encaminhados com sucesso');
     }
 
 }
